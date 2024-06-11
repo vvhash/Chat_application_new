02f2c4f3ed4c0318018c6e7e58f0a2a0778dc3db
vivermit <vivermit@gmail.com>
2024-06-11 13:51:01 +1000

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
+    port:5000,
     proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://chat-app-y7gf.onrender.com",
 			},
   },
 },
